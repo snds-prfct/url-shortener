ffe25d60cac1198d2fd777c1c307e3d0d6014af8
snds_prfct <snds.prfct@gmail.com>
2026-06-30 19:57:50 +0300

    Project Initialization

---
+++ src/test/java/dev/snds_prfct/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.snds_prfct.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
