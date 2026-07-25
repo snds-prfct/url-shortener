42a9512cb320298e5cd826f99b8fccf4ae61b98c
snds_prfct <snds.prfct@gmail.com>
2026-07-25 12:00:11 +0300

    API Gateway

---
+++ url-shortener-api-gateway/src/main/java/dev/snds_prfct/url_shortener/api_gateway/UrlShortenerApiGateway.java
@@ -0,0 +1,13 @@
+package dev.snds_prfct.url_shortener.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApiGateway.class, args);
+    }
+
+}
